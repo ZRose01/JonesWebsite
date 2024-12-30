7f9fce858f30ba0468b24fd29aa20c58de2d5b51
Zane Rosenberg <60989882+ZRose01@users.noreply.github.com>
2024-12-29 18:06:51 -0600

    more port

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
-const PORT = 0;
+const PORT = 80;
 
 // Serve static files from the "tS" directory
 app.use(express.static(path.join(__dirname, 'tS')));
 
-app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
-});
-
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'tS', 'land.html'));
+});
+
+app.listen(PORT, '0.0.0.0', (error) => {
+  if (error) {
+    console.error('Error starting server:', error);
+    return;
+  }
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
